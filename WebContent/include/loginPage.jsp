2ab342bad01b8e70509f9a67fd184de80c1824c6
zentim <zentim3248@gmail.com>
2018-09-28 21:53:26 +0800

    add login and logout function

--- WebContent/include/loginPage.jsp
+++ WebContent/include/loginPage.jsp
@@ -10,10 +10,17 @@
 		   		<p>Please enter your account and password</p>
 		   	</div>
 		   
-		    <form id="Login" action="LoginController" method="get">
+		    <form class="loginForm" action="forelogin" method="post">
+		
+				<div class="loginErrorMessageDiv" style="display: none;">
+	                <div class="alert alert-danger" role="alert">
+	                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+	                    <span class="errorMessage"></span>
+	                </div>        
+	            </div>
 		
 		        <div class="form-group">
-		            <input type="text" name="account" class="form-control" id="account" placeholder="Account">
+		            <input type="text" name="name" class="form-control" id="name" placeholder="Account">
 		        </div>
 		
 		        <div class="form-group">
@@ -23,7 +30,32 @@
 		       <button type="submit" class="btn btn-primary btn-lg" style="width: 100px;">Login</button>
 		       <p></p>
 			   <a href="./signUp.jsp">SignUp</a>
+			   <p></p>
 		    </form>
 	    </div>
 	</div>
 </div>
+
+<script>
+$(function(){
+     
+    <c:if test="${!empty msg}">
+	    $("span.errorMessage").html("${msg}");
+	    $("div.loginErrorMessageDiv").show();       
+    </c:if>
+     
+    $("form.loginForm").submit(function(){
+        if(0 == $("#name").val().length || 0 == $("#password").val().length){
+            $("span.errorMessage").html("請輸入帳號密碼");
+            $("div.loginErrorMessageDiv").show();           
+            return false;
+        }
+        return true;
+    });
+     
+    $("form.loginForm input").keyup(function(){
+        $("div.loginErrorMessageDiv").hide();   
+    });
+     
+})
+</script>
--- src/main/java/controller/servlet/ForeServlet.java
+++ src/main/java/controller/servlet/ForeServlet.java
@@ -1,15 +1,15 @@
 package main.java.controller.servlet;
 
-import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.util.HtmlUtils;
+
 import main.java.model.bean.Category;
+import main.java.model.bean.User;
 import main.java.model.dao.CategoryDAO;
 import main.java.model.dao.ProductDAO;
 import main.java.model.util.Page;
@@ -25,5 +25,44 @@ public class ForeServlet extends BaseForeServlet {
 		
 		return "home.jsp";
 	}
+	
+	public String register(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        name = HtmlUtils.htmlEscape(name);
+        boolean exist = userDAO.isExist(name);
+
+        if (exist) {
+            request.setAttribute("msg", "用戶名已經被使用，不能使用");
+            return "register.jsp";
+        }
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDAO.add(user);
+
+        return "@registerSuccess.jsp";
+    }
+	
+	public String login(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        name = HtmlUtils.htmlEscape(name);
+        String password = request.getParameter("password");
+
+        User user = userDAO.get(name, password);
+
+        if (null == user) {
+            request.setAttribute("msg", "帳號密碼錯誤");
+            return "login.jsp";
+        }
+        request.getSession().setAttribute("user", user);
+        return "@forehome";
+    }
+
+    public String logout(HttpServletRequest request, HttpServletResponse response, Page page) {
+        request.getSession().removeAttribute("user");
+        return "@forehome";
+    }
 
 }
