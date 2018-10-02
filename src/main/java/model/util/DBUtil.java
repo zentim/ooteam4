8f3f0b79714b46f8278b1c1e6e4b6f3b0d2a7c49
zentim <zentim3248@gmail.com>
2018-10-02 15:56:06 +0800

    edit database connection setting

--- src/main/java/model/util/DBUtil.java
+++ src/main/java/model/util/DBUtil.java
@@ -4,18 +4,27 @@ package main.java.model.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
 public class DBUtil {
 
-	// For mysql
-	public static String DBMS = "mysql";
-    static String ip = "192.168.99.100";
+    // For mysql with XAMPP
+    public static String DBMS = "mysql";
+    static String ip = "127.0.0.1";
     static int port = 3306;
     static String database = "ooteam4";
     static String encoding = "UTF-8";
     static String loginName = "root";
-    static String password = "root";
-    
+    static String password = "";
+
+	// For mysql with docker
+	// public static String DBMS = "mysql";
+ //    static String ip = "192.168.99.100";
+ //    static int port = 3306;
+ //    static String database = "ooteam4";
+ //    static String encoding = "UTF-8";
+ //    static String loginName = "root";
+ //    static String password = "root";
+
 	// For heroku postgresql
 //	public static String DBMS = "postgresql";
 //    static String ip = "ec2-174-129-18-98.compute-1.amazonaws.com";
@@ -24,7 +33,7 @@ public class DBUtil {
 //    static String encoding = "UTF-8";
 //    static String loginName = "iuaafgeuvfadox";
 //    static String password = "c649d081154e6b7ff215132c812938ee76131e4994fedfc9644ede854739411b";
- 
+
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -33,16 +42,16 @@ public class DBUtil {
             e.printStackTrace();
         }
     }
- 
+
     public static Connection getConnection() throws SQLException {
         String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
 //        String url = String.format("jdbc:postgresql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
         return DriverManager.getConnection(url, loginName, password);
     }
-     
+
     public static void main(String[] args) throws SQLException {
         System.out.println(getConnection());
-         
+
     }
- 
+
 }
