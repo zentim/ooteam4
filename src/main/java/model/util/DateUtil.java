1133e3e11c2883159315522b6274f4a038ff08bb
zentim <zentim3248@gmail.com>
2018-10-08 00:54:27 +0800

    add String to sql.date util function

--- src/main/java/model/util/DateUtil.java
+++ src/main/java/model/util/DateUtil.java
@@ -1,16 +1,25 @@
 package main.java.model.util;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 public class DateUtil {
-	 
+
     public static java.sql.Timestamp d2t(java.util.Date d) {
         if (null == d)
             return null;
         return new java.sql.Timestamp(d.getTime());
     }
- 
+
     public static java.util.Date t2d(java.sql.Timestamp t) {
         if (null == t)
             return null;
         return new java.util.Date(t.getTime());
+    }
+
+    public static java.sql.Date StringToSqlDate(String d) throws ParseException{
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date date = sdf1.parse(d);
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+        return sqlDate;
     }
 }
