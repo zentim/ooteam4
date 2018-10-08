15cc8d6db79f3ec7e2198a3ba9a7484dd8da2274
zentim <zentim3248@gmail.com>
2018-10-09 00:58:07 +0800

    change database name

--- src/main/java/model/util/DBUtil.java
+++ src/main/java/model/util/DBUtil.java
@@ -11,7 +11,7 @@ public class DBUtil {
     public static String DBMS = "mysql";
     static String ip = "127.0.0.1";
     static int port = 3306;
-    static String database = "ooteam4";
+    static String database = "mysql_shoppingcart";
     static String encoding = "UTF-8";
     static String loginName = "root";
     static String password = "";
