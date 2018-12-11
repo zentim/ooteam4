856dffb2e0217ba66332b59b8e05af9e850749c1
Zentim <zentim3248@gmail.com>
2018-12-11 13:11:35 +0800

    update decorator pattern comment

--- src/main/java/pattern/decorator/Decorator.java
+++ src/main/java/pattern/decorator/Decorator.java
@@ -2,6 +2,11 @@ package main.java.pattern.decorator;
 
 import main.java.pattern.composite.Component;
 
+/**
+ * 
+ * Decorator Pattern - Decorator
+ *
+ */
 public abstract class Decorator extends Component {
 
     public abstract void operation();
